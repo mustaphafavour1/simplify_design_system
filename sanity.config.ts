52f834d486459afb7d2fbc3ec8a965975aa51320
Favour <tek@Favours-MacBook-Pro.local>
2026-05-10 21:57:06 +0100

    fix: remove embedded studio to resolve React version conflicts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,21 +1,16 @@
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
-import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './sanity/schema'
 import { structure } from './sanity/structure'
 
 export default defineConfig({
   name: 'ss-design-system',
   title: 'SS Design System',
-
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
-
+  projectId: 'bjcr45l7',
+  dataset: 'production',
   plugins: [
     structureTool({ structure }),
-    visionTool(),
   ],
-
   schema: {
     types: schemaTypes,
   },
